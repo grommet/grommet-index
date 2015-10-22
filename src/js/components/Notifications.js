9c263a456daa1f184261e50dafefa5b1d76a14e3
Eric Soderberg <eric.soderberg@hp.com>
2015-10-21 17:52:02 -0700

    Simplified Notifications.

---
+++ src/js/components/Notifications.js
@@ -0,0 +1,42 @@
+// (C) Copyright 2014-2015 Hewlett-Packard Development Company, L.P.
+
+var React = require('react');
+var Notification = require('grommet/components/Notification');
+
+var CLASS_ROOT = 'resource-notifications';
+
+var Notifications = React.createClass({
+
+  propTypes: {
+    notifications: React.PropTypes.arrayOf(React.PropTypes.object)
+  },
+
+  render: function () {
+    var classes = [CLASS_ROOT];
+    if (this.props.className) {
+      classes.push(this.props.className);
+    }
+
+    var notifications;
+    if (this.props.notifications) {
+      notifications = this.state.notifications.map(function (notification) {
+        return (
+          <Notification key={notification.uri} flush={false}
+            status={notification.status}
+            message={notification.name}
+            state={notification.state}
+            timestamp={new Date(notification.created)} />
+        );
+      }, this);
+    }
+
+    return (
+      <div className={classes.join(' ')}>
+        {notifications}
+      </div>
+    );
+  }
+
+});
+
+module.exports = Notifications;
