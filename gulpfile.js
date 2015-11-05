a25a85e50e4a974fceb830cac0e2c9bb86c41c7d
Alan Souza <alansouzati@gmail.com>
2015-11-05 11:46:27 -0800

    Added CI integration to grommet-index

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,74 @@ var opts = {
   scsslint: true
 };
 
+gulp.task('release:createTmp', function(done) {
+  del.sync(['./tmp']);
+  mkdirp('./tmp', function(err) {
+    if (err) {
+      throw err;
+    }
+    done();
+  });
+});
+
+gulp.task('release:stable', ['dist', 'release:createTmp'], function(done) {
+  if (process.env.CI) {
+    git.clone('https://' + process.env.GH_TOKEN + '@github.com/grommet/grommet-index.git',
+      {
+        cwd: './tmp/'
+      },
+      function(err) {
+        if (err) {
+          throw err;
+        }
+
+        process.chdir('./tmp/grommet-index');
+        git.checkout('stable', function(err) {
+          if (err) {
+            throw err;
+          }
+
+          del.sync(['./**/*']);
+
+          gulp.src('../../dist/**').pipe(gulp.dest('./')).on('end', function() {
+            git.status({
+              args: '--porcelain'
+            }, function(err, stdout) {
+              if (err) {
+                throw err;
+              }
+
+              if (stdout && stdout !== '') {
+                gulp.src('./')
+                  .pipe(git.add({
+                    args: '--all'
+                  }))
+                  .pipe(git.commit('Stable dev version update.')).on('end', function() {
+                    git.push('origin', 'stable', { quiet: true }, function(err) {
+                      if (err) {
+                        throw err;
+                      }
+
+                      process.chdir(__dirname);
+                      done();
+                    });
+                  });
+              } else {
+                console.log('No difference since last commit, skipping stable release.');
+
+                process.chdir(__dirname);
+                done();
+              }
+            });
+          });
+        });
+      }
+    );
+  } else {
+    console.warn('Skipping release. Release:stable task should be executed by CI only.');
+  }
+});
+
 gulpTasks(gulp, opts);
 
 gulp.task('dev', function () {
