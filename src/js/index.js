052dac08089e1b70bbd306b6dac645f9b2579ab9
Alan Souza <alansouzati@gmail.com>
2016-03-28 20:46:29 -0700

    Changing the index.js to be used as regular import.

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,31 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
-export * from './components/Aggregate';
-export * from './components/Attribute';
-export * from './components/Filter';
-export * from './components/Filters';
-export * from './components/Header';
-export * from './components/History';
-export * from './components/Index';
-export * from './components/List';
-export * from './components/Table';
-export * from './components/Tiles';
-export * from './components/Timestamp';
-export * from './utils/PropTypes';
-export * from './utils/Query';
+import Aggregate from './components/Aggregate';
+import Attribute from './components/Attribute';
+import Filter from './components/Filter';
+import Filters from './components/Filters';
+import Header from './components/Header';
+import History from './components/History';
+import Index from './components/Index';
+import List from './components/List';
+import Table from './components/Table';
+import Tiles from './components/Tiles';
+import Timestamp from './components/Timestamp';
+import PropTypes from './utils/PropTypes';
+import Query from './utils/Query';
+
+export default {
+  Aggregate,
+  Attribute,
+  Filter,
+  Filters,
+  Header,
+  History,
+  Index,
+  List,
+  Table,
+  Tiles,
+  Timestamp,
+  PropTypes,
+  Query
+};
