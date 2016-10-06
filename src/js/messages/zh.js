4f2dee9a3995cad25c598cfb4d39342addf05833
shimrit-yacobi <shimrit.yacobi@hpe.com>
2016-10-06 13:45:50 -0600

    adding CN translation for the case of user selection "zh"

---
+++ src/js/messages/zh.js
@@ -0,0 +1,8 @@
+﻿// (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
+
+export default {
+  '无匹配项': '无匹配项',
+  Sort: '排序',
+  FilterSummary: '{count, number} 个值',
+  '筛选方式': '筛选方式'
+};
